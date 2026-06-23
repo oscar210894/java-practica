5e697340ec8c083a4125a70e3f56ed1750585016
Oscar Mendoza <oscar210894@gmail.com>
2026-06-23 12:22:26 -0600

    Primer commit: Clase Carro y Main

---
+++ src/Carro.java
@@ -0,0 +1,16 @@
+public class Carro  {
+
+    private final String marca;
+    private final String modelo;
+    private final int ano;
+
+    public Carro(String marca, String modelo, int ano){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public void mostrarInfo() {
+        System.out.println(marca + ", " + modelo + ", " + "Año: " + ano);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Carro toyota = new Carro("Toyota", "Corolla", 2026);
+        Carro mazda = new Carro("Mazda", "CX-5", 2023);
+
+        toyota.mostrarInfo();
+        mazda.mostrarInfo();
+    }
+}
